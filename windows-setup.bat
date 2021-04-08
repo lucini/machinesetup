771c65dc05fe343e0c0a6343a988051b3a3e62fd
Rafael Lucini <rafaellucini@gmail.com>
2021-04-07 21:47:49 -0300

    Java and maven

--- windows-setup.bat
+++ windows-setup.bat
@@ -8,7 +8,9 @@ choco install microsoft-teams -y
 choco install git -y
 choco install nodejs -y
 choco install postgresql13 --params '/Password:test' --ia '--serverport 5433' -y
+choco install openjdk11 -y
 
 choco install dbeaver -y
+choco install maven -y
 choco install vscode -y
 choco install intellijidea-community -y
