9b4252fcb9d451e157cb7878491d7cc1be910a16
Rafael Lucini <rafaellucini@gmail.com>
2021-04-07 21:13:11 -0300

    Update postgres password

--- windows-setup.bat
+++ windows-setup.bat
@@ -1,14 +1,14 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco install firefox
-choco install spotify
-choco install telegram
-choco install microsoft-teams
+choco install firefox -y
+choco install spotify -y
+choco install telegram -y
+choco install microsoft-teams -y
 
-choco install git
-choco install nodejs
-choco install postgresql
+choco install git -y
+choco install nodejs -y
+choco install postgresql13 --params '/Password:test' --ia '--serverport 5433' -y
 
-choco install dbeaver
-choco install vscode
-choco install intellijidea-community
+choco install dbeaver -y
+choco install vscode -y
+choco install intellijidea-community -y
