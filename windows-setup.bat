62a5516da186b8a3f68e2e966bdca9e27115dfee
Rafael Lucini <rafaellucini@gmail.com>
2021-04-12 10:17:11 -0300

    OpenSSL

--- windows-setup.bat
+++ windows-setup.bat
@@ -1,4 +1,7 @@
+#!/bin/bash
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+choco install openssl.light -y
 
 choco install firefox -y
 choco install spotify -y
